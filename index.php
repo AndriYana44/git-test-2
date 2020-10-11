e1ac4b7b3380faf850937f346b846a5ad6936f88
Andri Yana <andri.yana349@gmail.com>
2020-10-11 16:21:31 +0700

    Menambahkan keterangan

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
 </head>
 <body>
     <h3>Hello Wolrd!</h3>
+    <h5>Mencoba ramote git</h5>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam veritatis debitis, cum deleniti quia voluptatibus optio eos porro nam. Harum nobis voluptatum rem doloribus alias ea corrupti officia, cumque aliquid?</p>
     <ul>
         <li>satu</li>
         <li>dua</li>
+        <li>tiga</li>
     </ul>
 </body>
 </html>
